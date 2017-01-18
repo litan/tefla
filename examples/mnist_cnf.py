3b2aac400ca9c8d21495cd8d796edb6984c13645
Lalit Pant <pant.lalit@gmail.com>
2017-01-18 11:33:38 +0530

    Move 'iterator_type' and 'sample_every' to training config.

--- examples/mnist_cnf.py
+++ examples/mnist_cnf.py
@@ -3,16 +3,15 @@ from __future__ import division, print_function, absolute_import
 import tensorflow as tf
 
 from tefla.core.lr_policy import StepDecayPolicy
-from tefla.utils import util
 from tefla.da.standardizer import SamplewiseStandardizer
+from tefla.utils import util
 
 cnf = {
     'name': __name__.split('.')[-1],
+    'classification': True,
+    'iterator_type': 'parallel',  # parallel or queued
     'batch_size_train': 128,
     'batch_size_test': 128,
-    'classification': True,
-    'standardizer': SamplewiseStandardizer(clip=6),
-    'validation_scores': [('validation accuracy', util.accuracy_wrapper), ('validation kappa', util.kappa_wrapper)],
     'aug_params': {
         'zoom_range': (1 / 1.05, 1.05),
         'rotation_range': (-5, 5),
@@ -21,12 +20,15 @@ cnf = {
         'do_flip': False,
         'allow_stretch': True,
     },
+    'standardizer': SamplewiseStandardizer(clip=6),
     'num_epochs': 30,
+    'summary_every': 5,
     'lr_policy': StepDecayPolicy(
         schedule={
             0: 0.001,
             15: 0.0001,
         }
     ),
-    'optimizer': tf.train.AdamOptimizer()
+    'optimizer': tf.train.AdamOptimizer(),
+    'validation_scores': [('validation accuracy', util.accuracy_wrapper), ('validation kappa', util.kappa_wrapper)],
 }
