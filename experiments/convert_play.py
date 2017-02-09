86bcdad6720bb9ffb29ca0e000cd9f977ac9ff5a
siddhant <siddhant@siddhant>
2017-02-09 18:17:01 +0530

    Added a crop image function which can work on any mono-color background.

--- experiments/convert_play.py
+++ experiments/convert_play.py
@@ -1,13 +1,18 @@
-from PIL import Image, ImageFilter
 
+from PIL import Image, ImageFilter
+import cv2
 import matplotlib.patches as mpatches
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib.gridspec as gridspec
 from skimage import filters
 from skimage import measure
 from skimage.color import rgb2gray
 from skimage.morphology import closing, square
 from tefla.da import data
+import os
+import shutil
+plt.get_backend()
 
 
 def convert(fname, target_size):
@@ -96,10 +101,72 @@ def convert_new_regions(fname, target_size):
     return ba
 
 
+def find_boundary_reverse(count,size):
+    retval=count.size-1
+    if count[count.size-1]==size or count[count.size-1]==0:
+        for i in range(np.array(count).size-2, 0, -1):
+            if(count[i] != count[i+1]):
+                retval=i
+                break
+    return retval
+
+def find_boundary(count,size):
+    retval=0
+    if count[0]==size or count[0]==0:
+        for i in range(0,np.array(count).size-2, 1):
+            if(count[i] != count[i+1]):
+                retval=i
+                break
+    return retval
+
+def subplots(img,row_count,col_count,crop_img):
+    gs = gridspec.GridSpec(2, 2, width_ratios=[4, 3])
+    plt.subplot(gs[0])
+    imshow(plt, img)
+
+    plt.subplot(gs[1])
+    plt.plot(np.array(row_count))
+
+    plt.subplot(gs[2])
+    plt.plot(np.array(col_count))
+
+    plt.subplot(gs[3])
+    imshow(plt,crop_img)
+    plt.show()
+
+#crops an image from both dark and light background
+#works best on a single color background
+def crop_image(fname):
+    print('Processing image: %s' % fname)
+
+    #otsu thresholding
+    img = Image.open(fname)
+    blurred = img.filter(ImageFilter.BLUR)
+    ba = np.array(blurred)
+    gray_image = cv2.cvtColor(ba, cv2.COLOR_BGR2GRAY)
+    retval2, threshold2 = cv2.threshold(gray_image, 125, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    #storing white pixel in  each row and column in two arrays
+    #these arrays are later used to find boundaries for cropping image
+    row_white_pixel_count=np.count_nonzero(threshold2,axis=1)
+    col_white_pixel_count=np.count_nonzero(threshold2,axis=0)
+
+    #find x,y,w,h for cropping image
+    y=find_boundary(row_white_pixel_count,col_white_pixel_count.size)
+    h=find_boundary_reverse(row_white_pixel_count,col_white_pixel_count.size)
+    x=find_boundary(col_white_pixel_count,row_white_pixel_count.size)
+    w=find_boundary_reverse(col_white_pixel_count,row_white_pixel_count.size)
+    crop_img = ba[y:h, x:w]
+
+    #uncomment below line to see histogram of both white pixel vs rows and white pixel vs columns
+    #subplots(threshold2, row_white_pixel_count, col_white_pixel_count, crop_img)
+    return crop_img
+
+
 def draw_top_regions(properties, n0):
     n = min(n0, len(properties))
     print('Drawing %d regions' % n)
-    colors = ['red', 'red', 'red']
+    colors = ['red', 'blue', 'green']
     for i in range(n):
         region = properties[i]
         minr, minc, maxr, maxc = region.bbox
@@ -107,6 +174,8 @@ def draw_top_regions(properties, n0):
         rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
                                   fill=False, edgecolor=colors[i % len(colors)], linewidth=1)
         plt.gca().add_patch(rect)
+        return minr, minc, maxr, maxc
+
 
 
 def imshow(plt, image):
@@ -114,7 +183,7 @@ def imshow(plt, image):
     plt.imshow(show_img, cmap='gray')
 
 
-images_dir = '/media/lalit/data/eyepacs-dr/make-testset/viz/images'
+images_dir = '/home/siddhant/work/img'
 
 files = data.get_image_files(images_dir)
 original_images = data.load_images(files)
@@ -130,11 +199,11 @@ for j, file in enumerate(files):
     imshow(plt, img)
 
     plt.subplot(num, num_cols, j * num_cols + 3)
-    img = convert_new(file, 512)
+    img = convert_new_regions(file, 512)
     imshow(plt, img)
 
     plt.subplot(num, num_cols, j * num_cols + 4)
-    img = convert_new_regions(file, 512)
+    img = crop_image(file)
     imshow(plt, img)
 
 plt.show()
